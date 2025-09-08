882d79f80809a3e59eff9d6e13e9f20a63f9fe41
banhbao2 <trong_nghia.hoang@outlook.de>
2025-09-08 23:00:53 +0200

    Initial Commit

---
+++ ruevGG/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ruevGG
+//
+//  Created by Nghia on 08.09.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ruevGG/ruevGGApp.swift
@@ -0,0 +1,17 @@
+//
+//  ruevGGApp.swift
+//  ruevGG
+//
+//  Created by Nghia on 08.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct ruevGGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
